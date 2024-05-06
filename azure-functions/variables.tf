2432ee6ee6473e8cf578c4e3a4e87bb294187bcc
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-06 16:49:44 +0200

    variables.tf

--- azure-functions/variables.tf
+++ azure-functions/variables.tf
@@ -1,1 +1,25 @@
+variable "resource_group_name" {
+  type    = string
+  default = "example-rg"
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = "examplestorageaccount"
+}
+
+variable "location" {
+  type    = string
+  default = "West Europe"
+}
+
+variable "service_plan_name" {
+  type    = string
+  default = "example-service-plan"
+}
+
+variable "function_app_name" {
+  type    = string
+  default = "example-function-app"
+}
 
